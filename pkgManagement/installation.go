d8d9bdc523282fe10c14b73d1014618434d6a6b8
pezhman <github@pezhmankasraee.com>
2025-06-23 15:06:38 +0200

    fix bug on reinstall

--- pkgManagement/installation.go
+++ pkgManagement/installation.go
@@ -25,14 +25,16 @@ func install(application *model.Application, basePath string) {
 
 	appLowerName := strings.ToLower(application.Name)
 	fmt.Println("")
-	pklog.CreateLog(pklog.Information, "start installing [   "+appLowerName+"   ] ...")
+	pklog.CreateLog(pklog.Information, fmt.Sprintf("start installing [   %s   ] ...", appLowerName))
 
 	applicationBasePath := basePath + "/" + application.Name
 	_, err := os.Stat(applicationBasePath)
-	if err != nil {
-		pklog.CreateLog(pklog.Warning, "path to "+applicationBasePath+" does not exist")
-		ioutility.MakeDirectory(applicationBasePath)
+	if err == nil {
+		os.RemoveAll(applicationBasePath)
 	}
+
+	ioutility.MakeDirectory(applicationBasePath)
+
 	net.Download(application, applicationBasePath)
 
 	if err := uncompressFiles(application, applicationBasePath); err != nil {
@@ -44,13 +46,13 @@ func uncompressFiles(application *model.Application, applicationBasePath string)
 	destination := applicationBasePath + "/" + application.Filename
 
 	if strings.HasSuffix(application.Filename, ".tar.gz") {
-		pklog.CreateLog(pklog.Information, "extracting .tar.gz: "+destination)
+		pklog.CreateLog(pklog.Information, fmt.Sprintf("extracting .tar.gz: %s", destination))
 		if err := ioutility.ExtractTarGz(destination); err != nil {
 			return fmt.Errorf(".tar.gz file: %s", err.Error())
 		}
 		pklog.CreateLog(pklog.Information, "extracted successfully.")
 	} else if strings.HasSuffix(application.Filename, ".zip") {
-		pklog.CreateLog(pklog.Information, "extracting .zip: "+destination)
+		pklog.CreateLog(pklog.Information, fmt.Sprintf("extracting .zip: %s", destination))
 		if err := ioutility.ExtractZip(destination); err != nil {
 			return fmt.Errorf(".zip file: %s", err.Error())
 		}
